11adfd5f21715318e158f05dd1ffcaf1e8282fe6
Márton Horváth <hmartonsandor@gmail.com>
2024-04-01 18:53:06 +0200

    refactor ApplyForCitizenshipWarning component

---
+++ src/components/alerts/applyForCitizenshipWarning/ApplyForCitizenshipWarning.tsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+import { Alert, Link } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const ApplyForCitizenshipWarning = () => {
+  const navigate = useNavigate();
+
+  const StyledLink = styled(Link)`
+    font-family: 'Source Sans Pro';
+    color: #663c00;
+    padding: 4px 0;
+    cursor: pointer;
+  `;
+
+  return (
+    <Alert
+      sx={{ margin: '10px auto' }}
+      severity='warning'
+      action={
+        <StyledLink onClick={() => navigate('profile')}>
+          Apply for citizenship
+        </StyledLink>
+      }
+    >
+      You need cizizenship to be part of the community!
+    </Alert>
+  );
+};
+
+export default ApplyForCitizenshipWarning;
--- src/components/page/main/Main.tsx
+++ src/components/page/main/Main.tsx
@@ -1,27 +1,18 @@
 import * as React from 'react';
-import { Alert, Box, Link } from '@mui/material';
+import { Box } from '@mui/material';
 import { MainProps } from './types';
 import useRenderMainContentCheck from '@hooks/check/useRenderMainContentCheck';
 import { useUserContext } from '@hooks/context/userContext/UserContext';
 import { USER_MODES } from '@global/types/user';
-import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
+import ApplyForCitizenshipWarning from '@components/alerts/applyForCitizenshipWarning/ApplyForCitizenshipWarning';
 
 const Main: React.FC<MainProps> = ({ children }) => {
   const { userState } = useUserContext();
-  const navigate = useNavigate();
   const { renderMainContent } = useRenderMainContentCheck();
 
   if (!renderMainContent) {
     return null;
   }
-
-  const StyledLink = styled(Link)`
-    font-family: 'Source Sans Pro';
-    color: #663c00;
-    padding: 4px 0;
-    cursor: pointer;
-  `;
 
   return (
     <Box
@@ -33,19 +24,7 @@ const Main: React.FC<MainProps> = ({ children }) => {
         alignItems: 'center'
       }}
     >
-      {userState.mode === USER_MODES.GUEST && (
-        <Alert
-          sx={{ margin: '10px auto' }}
-          severity='warning'
-          action={
-            <StyledLink onClick={() => navigate('profile')}>
-              Apply for citizenship
-            </StyledLink>
-          }
-        >
-          You need cizizenship to be part of the community!
-        </Alert>
-      )}
+      {userState.mode === USER_MODES.GUEST && <ApplyForCitizenshipWarning />}
       {children}
     </Box>
   );
