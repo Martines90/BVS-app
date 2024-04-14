b89b6a6a1fceff5ed805bc5307b491aa346970e1
Márton Horváth <marton.horvath@adevinta.com>
2024-04-14 09:23:22 +0200

    add test case cover successful application

--- src/pages/citizens/apply-for-citizenship/ApplyForCitizenshipPage.test.tsx
+++ src/pages/citizens/apply-for-citizenship/ApplyForCitizenshipPage.test.tsx
@@ -1,3 +1,5 @@
+import { getBytes32keccak256Hash } from '@global/helpers/hash-manipulation';
+import userEvent from '@testing-library/user-event';
 import { act, render, screen } from 'test-utils';
 import ApplyForCitizenshipPage from './ApplyForCitizenshipPage';
 
@@ -19,7 +21,7 @@ describe('ApplyForCitizenshipPage', () => {
   });
 
   describe('user with no citizenship and not applied for citizenship state', () => {
-    it('should render default view', async () => {
+    it('should render expected view', async () => {
       await act(() => {
         render(<ApplyForCitizenshipPage />);
       });
@@ -37,17 +39,37 @@ describe('ApplyForCitizenshipPage', () => {
       expect(mockContractFunctions.getCitizenRoleApplicationFee).toHaveBeenCalled();
     });
 
-    it('should execute application when clicked on Apply for citizenship button', async () => {
+    it('should call applyForCitizenshipRole when clicked on Apply for citizenship button', async () => {
+      const mockWallerAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+      const wrapperProps = { initUserState: { walletAddress: mockWallerAddress } };
 
+      let container: any;
+      await act(async () => {
+        ({ container } = render(<ApplyForCitizenshipPage />, { wrapperProps }));
+      });
+
+      const emailInput = container.querySelector('input[name="email"]');
+
+      await userEvent.type(emailInput, 'test@email.com');
+
+      await userEvent.click(screen.getByRole('button', { name: 'Apply for citizenship' }));
+
+      const expectedHash = getBytes32keccak256Hash(`test@email.com${mockWallerAddress}`);
+
+      expect(
+        mockContractFunctions.applyForCitizenshipRole
+      ).toHaveBeenCalledWith(expectedHash, 10000);
     });
   });
 
   describe('user who already has citizenship role', () => {
     it('should render expected view', async () => {
+      const wrapperProps = { initUserState: { walletAddress: '0x0' } };
+
       mockContractFunctions.hasRole.mockImplementation(() => true);
 
       await act(() => {
-        render(<ApplyForCitizenshipPage />, { wrapperProps: { initUserState: { walletAddress: '0x0' } } });
+        render(<ApplyForCitizenshipPage />, { wrapperProps });
       });
 
       expect(screen.queryByText('Your citizen role has already been granted!')).toBeInTheDocument();
