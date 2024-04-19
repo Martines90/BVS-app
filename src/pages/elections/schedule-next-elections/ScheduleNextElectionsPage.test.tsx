b135080bbc0dee050731231e064dfe404354c030
Márton Horváth <marton.horvath@adevinta.com>
2024-04-19 10:14:04 +0200

    add there is ongoing elections test case

--- src/pages/elections/schedule-next-elections/ScheduleNextElectionsPage.test.tsx
+++ src/pages/elections/schedule-next-elections/ScheduleNextElectionsPage.test.tsx
@@ -29,41 +29,52 @@ describe('ScheduleNextElectionsPage', () => {
     expect(ScheduleNextElectionsPage).toBeDefined();
   });
 
-  describe('elections not yet scheduled', () => {
-    it('should display properly date pickers enabled / disabled state', async () => {
-      await act(async () => {
-        render(<ScheduleNextElectionsPage />);
-      });
-
-      expect(mockContractFunctions.getElectionStartEndIntervalInDays).toHaveBeenCalled();
-      expect(mockContractFunctions.isThereOngoingElections).toHaveBeenCalled();
-
-      expect(
-        screen.queryByText('Schedule Next Elections')
-      ).toBeInTheDocument();
-
-      const startDateInput = screen.getByTestId('start-date');
-
-      await userEvent.click(startDateInput);
-
-      await userEvent.click(screen.getByText('25'));
-
-      expect(screen.getByDisplayValue('25/05/2024')).toHaveAttribute('name', 'start-date-field');
-
-      const endDateInput = screen.getByTestId('end-date');
-
-      await userEvent.click(endDateInput);
-
-      await userEvent.click(screen.getByText('28'));
-
-      expect(screen.getByDisplayValue('28/06/2024')).toHaveAttribute('name', 'end-date-field');
-
-      await userEvent.click(screen.getByRole('button', { name: 'SCHEDULE ELECTION' }));
-
-      expect(mockContractFunctions.scheduleNextElections).toHaveBeenCalled();
-
-      expect(screen.queryByText('There is an already scheduled or ongoing election!', { exact: false })).toBeInTheDocument();
-      expect(screen.queryByText('Check out elections', { exact: false })).toBeInTheDocument();
+  it('should display properly date pickers enabled / disabled state when elections not yet scheduled', async () => {
+    await act(async () => {
+      render(<ScheduleNextElectionsPage />);
     });
+
+    expect(mockContractFunctions.getElectionStartEndIntervalInDays).toHaveBeenCalled();
+    expect(mockContractFunctions.isThereOngoingElections).toHaveBeenCalled();
+
+    expect(
+      screen.queryByText('Schedule Next Elections')
+    ).toBeInTheDocument();
+
+    const startDateInput = screen.getByTestId('start-date');
+
+    await userEvent.click(startDateInput);
+
+    await userEvent.click(screen.getByText('25'));
+
+    expect(screen.getByDisplayValue('25/05/2024')).toHaveAttribute('name', 'start-date-field');
+
+    const endDateInput = screen.getByTestId('end-date');
+
+    await userEvent.click(endDateInput);
+
+    await userEvent.click(screen.getByText('28'));
+
+    expect(screen.getByDisplayValue('28/06/2024')).toHaveAttribute('name', 'end-date-field');
+
+    await userEvent.click(screen.getByRole('button', { name: 'SCHEDULE ELECTION' }));
+
+    expect(mockContractFunctions.scheduleNextElections).toHaveBeenCalled();
+
+    expect(screen.queryByText('There is an already scheduled or ongoing election!', { exact: false })).toBeInTheDocument();
+    expect(screen.queryByText('Check out elections', { exact: false })).toBeInTheDocument();
+  });
+
+  it('should only show warning info telling elections already scheduled when elections already scheduled', async () => {
+    mockContractFunctions.isThereOngoingElections.mockImplementationOnce(
+      () => Promise.resolve(true)
+    );
+
+    await act(async () => {
+      render(<ScheduleNextElectionsPage />);
+    });
+
+    expect(screen.queryByText('There is an already scheduled or ongoing election!', { exact: false })).toBeInTheDocument();
+    expect(screen.queryByText('Check out elections', { exact: false })).toBeInTheDocument();
   });
 });
